81f3377169e70922c2bc37ecde5d2a0e2bd39574
Towsib <rayhan15-4164@diu.edu.bd>
2022-12-09 23:58:13 +0600

    bio

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,33 @@
-import dynamic from "next/dynamic";
 import Head from "next/head";
-import About from "../src/components/About";
+import BIO from "../src/components/BIO";
+import RESEARCH from "../src/components/RESEARCH";
+import PUBLICATIONS from "../src/components/PUBLICATIONS";
+import TEACHING from "../src/components/TEACHING";
 import Contact from "../src/components/Contact";
 import Home from "../src/components/Home";
-import News from "../src/components/News";
-import Newsletter from "../src/components/Newsletter";
-import Partners from "../src/components/Partners";
-import Process from "../src/components/Process";
-import Service from "../src/components/Service";
-import Skills from "../src/components/Skills";
-import Testimonial from "../src/components/Testimonial";
 import Layout from "../src/layout/Layout";
-
-const Portfolio = dynamic(() => import("../src/components/Portfolio"), {
-  ssr: false,
-});
 
 const Index = () => {
   return (
     <Layout>
       <Head>
-        <title>Dizme | Home</title>
+        <title>Stanford | PROFILES</title>
       </Head>
       <Home />
       {/* HERO */}
       {/* PROCESS */}
-      <Process />
-      {/* /PROCESS */}
-      {/* ABOUT */}
-      <About />
-      {/* /ABOUT */}
-      {/* PORTFOLIO */}
-      <Portfolio />
-      {/* /PORTFOLIO */}
-      {/* SKILLS */}
-      <Skills />
-      {/* /SKILLS */}
-      {/* SERVICES */}
-      <Service />
-      {/* /SERVICES */}
-      {/* TESTIMONIALS */}
-      <Testimonial />
-      {/* /TESTIMONIALS */}
-      {/* PARTNERS */}
-      <Partners />
-      {/* /PARTNERS */}
-      {/* NEWS */}
-      <News />
-      <Newsletter />
+      <BIO />
+      {/* /BIO */}
+      {/* BIO */}
+      <RESEARCH />
+      {/* /RESEARCH */}
+      {/* RESEARCH */}
+      <PUBLICATIONS />
+      {/* /PUBLICATIONS  */}
+      {/* PUBLICATIONS  */}
+      <TEACHING />
+      {/* /TEACHING  */}
+      {/* TEACHING  */}
       <Contact />
     </Layout>
   );
